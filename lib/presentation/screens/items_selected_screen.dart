e11bc9fd6af2f2b8c186ed589326fc5b4d470ca7
Eduardo Canchola <canchola700@gmail.com>
2024-05-23 00:44:42 -0600

    Screen opciones seleccionadas

---
+++ lib/presentation/screens/items_selected_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class ItemsSelectedScreen extends StatelessWidget {
+  
+  const ItemsSelectedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('kjhhjhkj'),
+    );
+  }
+}
